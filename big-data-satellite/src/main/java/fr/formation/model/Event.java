803590c1c35f6efa8d3ef45f39cffd824803911a
sawsan <bastaoui.saousane@gmail.com>
2024-05-24 16:09:46 +0200

    Event .class

---
+++ big-data-satellite/src/main/java/fr/formation/model/Event.java
@@ -0,0 +1,40 @@
+package fr.formation.model;
+
+public class Event {
+    
+
+private String date ; 
+
+private double bz; 
+
+private double variationMagnetique;
+
+public String getDate() {
+    return date;
+}
+
+public void setDate(String date) {
+    this.date = date;
+}
+
+public double getBz() {
+    return bz;
+}
+
+public void setBz(double bz) {
+    this.bz = bz;
+}
+
+public double getVariationMagnetique() {
+    return variationMagnetique;
+}
+
+public void setVariationMagnetique(double variationMagnetique) {
+    this.variationMagnetique = variationMagnetique;
+} 
+
+
+
+
+    
+}
