cff137971245a69508457040600c75bd8c7fb03a
HediB <hedibardou@gmail.com>
2024-05-24 18:14:44 +0200

    modif EventMapper

--- big-data-satellite/src/main/java/fr/formation/EventMapper.java
+++ big-data-satellite/src/main/java/fr/formation/EventMapper.java
@@ -4,24 +4,31 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
 
-public class EventMapper extends Mapper<LongWritable, Text, Text, Text>{
-     
-        @Override
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            
+
+public class EventMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text>{
+
+    @Override
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+            throws IOException {
+        
             String[] parts = value.toString().split(";");
             
             String date = parts[0];
-            //double vitesseVent = Double.parseDouble(parts[1]);
+            double speed = Double.parseDouble(parts[1]);
+            double density = Double.parseDouble(parts[2]);
             double bz = Double.parseDouble(parts[3]);
             double magneticVariation = Double.parseDouble(parts[4]);
-    
+        
             if (bz < -20 || Math.abs(magneticVariation) > 1000) {
-                context.write(new Text(date.substring(0, 4)), value); // utilise annee comme cle
+                output.collect(new Text(date.substring(0, 4)), value); // utilise annee comme cle
             }
-        }
+            
+    }            
 }
 
 
